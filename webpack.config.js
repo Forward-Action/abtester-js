330a57a1c84c537cfb992cc4b81a48c9b3d2a9c5
LewJ <lew@lewj.me>
2020-04-28 13:04:44 +0100

    upgraded packages, switched uglify for terser minify should fix security issues

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,46 @@
-var webpack = require('webpack');
-var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-var path = require('path');
+var webpack = require('webpack')
+const TerserPlugin = require('terser-webpack-plugin')
+var path = require('path')
 
 module.exports = (env, options) => {
-  const inProduction = options.mode === 'production';
+    const inProduction = options.mode === 'production'
 
-  return {
-      entry: ['./src/main.js'],
-      output: {
-          path: path.resolve(__dirname, './dist'),
-          filename: inProduction  ? 'abtester.min.js' : 'abtester.js',
-          libraryTarget: 'var',
-          library: 'ABTester'
-      },
-      module: {
-          rules: [
-              {
-                  test: /\.js$/,
-                  exclude: /node_modules/,
-                  loader: 'babel-loader',
-                  query: {
-                      presets: ['env']
-                  }
-              },
-          ]
-      },
-      plugins: [
-          new webpack.LoaderOptionsPlugin({
-              minimize: inProduction
-          }),
-      ],
-      optimization: {
-          minimizer: [
-            new UglifyJsPlugin({
-              cache: true,
-              parallel: true,
-              uglifyOptions: {
-              compress: false,
-              ecma: 6,
-              mangle: true
-              },
-              sourceMap: true
-          })]
-      }
-  }
+    return {
+        entry: ['./src/main.js'],
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: inProduction ? 'abtester.min.js' : 'abtester.js',
+            libraryTarget: 'var',
+            library: 'ABTester',
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env'],
+                },
+            }, ],
+        },
+        plugins: [
+            new webpack.LoaderOptionsPlugin({
+                minimize: inProduction,
+            }),
+        ],
+        optimization: {
+            minimize: true,
+            minimizer: [
+                new TerserPlugin({
+                    test: /\.js(\?.*)?$/i,
+                    terserOptions: {
+                        output: {
+                            comments: false,
+                        },
+                    },
+                    extractComments: false
+                }),
+            ],
+        },
+    }
 }
